22342b8e85984721a01929f79636a91681e5aaa9
DamienPelaez <dpdprof@gmail.com>
2023-04-08 19:23:47 +0200

    Terminé

--- DbAppCli/Controller/ClientController.cs
+++ DbAppCli/Controller/ClientController.cs
@@ -7,7 +7,7 @@ public class ClientController: Factory
     private MySqlConnection _connection = init();
     
     //Cette méthode liste tout les clients de la banque
-    public void GetClients()
+    public void getClients()
     {
         string query = "SELECT * FROM client";
 
@@ -19,10 +19,11 @@ public class ClientController: Factory
         {
             Console.WriteLine("ID : "+reader.GetString(0)+" ; first_name : "+reader.GetString(1)+" ; last_name : "+reader.GetString(2)+" ; email : "+reader.GetString(3)+" ; phone : "+reader.GetString(4));
         }
+        closeReader(reader);
     }
     
     //Cette méthode list un client selon un critère bien défini
-    public void GetClient(string selector, string selectorValue)
+    public void getClient(string selector, string selectorValue)
     {
         //selector est l'option de la clause WHERE et value la valeur de la clause.
         string query = "SELECT * FROM client";
@@ -38,7 +39,7 @@ public class ClientController: Factory
                 query += " WHERE phone = " + selectorValue;
                 break;
             default:
-                GetClients();
+                getClients();
                 break;
         }
         
@@ -48,10 +49,57 @@ public class ClientController: Factory
         {
             Console.WriteLine("ID : "+reader.GetString(0)+" ; first_name : "+reader.GetString(1)+" ; last_name : "+reader.GetString(2)+" ; email : "+reader.GetString(3)+" ; phone : "+reader.GetString(4));
         }
+        closeReader(reader);
+    }
+    
+    //Crée un nouveau client
+    public void createClient(string[] values)
+    {
+        if (values.Length-1 != 3)
+        {
+            Console.WriteLine("Erreur trop ou trop peu de champs");
+        }
+        else
+        {
+            string query = "INSERT INTO client (first_name, last_name, email, phone) VALUES ('" + MySqlHelper.EscapeString(values[0]) + "','" + MySqlHelper.EscapeString(values[1]) + "','" + MySqlHelper.EscapeString(values[2]) + "','" + MySqlHelper.EscapeString(values[3]) + "')";
+            MySqlCommand command = new MySqlCommand(query, _connection);
+            command.ExecuteNonQuery();
+            //values 2 est l'email
+            getClient("email", values[2]);
+        }
+    }
+    
+    //Cette fonction met à jour les données first_name et last_name d'un client
+    public void updateClient(int id, string[] values)
+    {
+        string query = "UPDATE client SET first_name = '" + values[0] + "', last_name = '"+values[1]+"' WHERE ID = "+id;
+        MySqlCommand command = new MySqlCommand(query, _connection);
+
+        query = "SELECT * FROM client WHERE ID = " + id;
+        
+        MySqlCommand command2 = new MySqlCommand(query, _connection);
+        MySqlDataReader reader = command2.ExecuteReader();
+
+        while (reader.Read())
+        {
+            Console.WriteLine("ID : "+reader.GetString(0)+" ; first_name : "+reader.GetString(1)+" ; last_name : "+reader.GetString(2)+" ; email : "+reader.GetString(3)+" ; phone : "+reader.GetString(4));
+        }
+        closeReader(reader);
     }
 
-    public void update(string id, string[] value)
+    public void deleteClient(int id)
     {
-        
+        string query = "DELETE FROM client WHERE ID = " + id;
+        MySqlCommand command = new MySqlCommand(query, _connection);
+        command.ExecuteNonQuery();
+        getClients();
+    }
+
+    public void closeReader(MySqlDataReader reader)
+    {
+        if (reader != null)
+        {
+            reader.Close();
+        }
     }
 }
--- DbAppCli/Program.cs
+++ DbAppCli/Program.cs
@@ -5,8 +5,53 @@ Console.WriteLine("MySQL - Damien Pelaez-Diaz");
 /*
  * Les commandes à éxecuter afin d'avoir les résultat sur programme se
  * trouve dans cmd.sql
+ *
+ * Je n'ai pas poussé trop les choses pour pas perdre du temps inutilement.
+ * Je par donc du principe qu'il n'y à pas de faute de frappes ou d'erreur commise par l'utilisateur
+ *
+ * Les commandes sont également prédéfini
 */
 
 ClientController clientController = new ClientController();
-//clientController.GetClients();
-clientController.GetClient("email", "bob.martin@example.com");
+string cmd;
+while (true)
+{
+ cmd = "";
+ Console.WriteLine("\nAffichez tout les clients : 1");
+  Console.WriteLine("Affiche un client : 2");
+  Console.WriteLine("Créer un client : 3");
+  Console.WriteLine("Modifier un client : 4");
+  Console.WriteLine("Supprimez un client : 5");
+  Console.Write("You C> ");
+  try
+  {
+   cmd = Console.ReadLine();
+   switch (cmd)
+   {
+    case "1":
+     clientController.getClients();
+     break;
+    case "2":
+     clientController.getClient("email", "bob.martin@example.com");
+     break;
+    case "3":
+     string[] values = new string[] { "Gérard", "Junio" };
+     clientController.updateClient(1, values);
+     break;
+    case "4":
+     string[] data = new string[] { "Jeanne", "D'arc", "jeannebrule@gmail.com", "0123456789" };
+     clientController.createClient(data);
+     break;
+    case "5":
+     clientController.deleteClient(1);
+     break;
+    default:
+     clientController.getClients();
+     break;
+   }
+  }
+  catch (Exception e)
+  {
+   Console.WriteLine("Erreur : " + e.Message);
+  }
+}
